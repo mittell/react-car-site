a06e1713557ad0be5a466133c62793e1159b6069
Chris Mittell <mittell@hotmail.co.uk>
2022-09-07 11:52:54 +0900

    feat: added Options component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import Handling from './components/Handling/Handling';
 import Navbar from './components/Navbar/Navbar';
+import Options from './components/Options/Options';
 import Power from './components/Power/Power';
+import Speed from './components/Speed/Speed';
 
 function App() {
 	return (
 		<>
 			<Navbar />
 			<Power />
+			<Speed />
+			<Handling />
+			<Options />
 		</>
 	);
 }
